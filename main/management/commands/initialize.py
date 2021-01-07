26e4a087058475fe796c0827580318ac1be1c9ad
Mernus <mernusv@gmail.com>
2021-01-08 01:05:56 +0300

    Init command

--- main/management/commands/initialize.py
+++ main/management/commands/initialize.py
@@ -1,15 +1,57 @@
 import os
 
 from django.contrib.auth import get_user_model
+from django.core.management import call_command, BaseCommand
 
-from django.core.management import call_command
-from django.core.management.base import BaseCommand
+from main.utils import colored_print as _print
 
 
 class Command(BaseCommand):
-    help = "Creates an admin user non-interactively if it doesn't exist"
+    help = "Runs base Django commands for project preparation"
 
     def handle(self, *args, **options):
-        user = get_user_model()
-        if not user.objects.filter(username=os.getenv("DJANGO_SUPERUSER_USERNAME")).exists():
-            call_command("createsuperuser", interactive=False)
+        _print("Running migrations.", string_code="info", path="initialize")
+
+        try:
+            call_command("makemigrations", interactive=False)
+            call_command("migrate", interactive=False)
+        except Exception as exc:
+            _print(str(exc), string_code="err", path="initialize")
+            _print("Migrations failed.", string_code="err", path="initialize", exit=True)
+
+        _print("Migrations passed successfully.", string_code="success", path="initialize")
+        _print("Running collectstatic.", string_code="info", path="initialize")
+
+        try:
+            call_command("collectstatic", interactive=False)
+        except Exception as exc:
+            _print(str(exc), string_code="err", path="initialize")
+            _print("Collect static files failed.", string_code="err", path="initialize", exit=True)
+
+        _print("Static files were collect successfully.", string_code="success", path="initialize")
+
+        user_model = get_user_model()
+        superuser_username = os.getenv("DJANGO_SUPERUSER_USERNAME", "admin")
+        superuser = user_model.objects.filter(username=superuser_username).first()
+
+        if not superuser:
+            _print("Creating superuser.", string_code="info", path="initialize")
+
+            try:
+                call_command("createsuperuser", interactive=False)
+            except Exception as exc:
+                _print(str(exc), string_code="warn", path="initialize")
+                _print("Superuser creation failed.", string_code="warn", path="initialize")
+            else:
+                _print("Superuser created successfully.", string_code="success", path="initialize")
+        else:
+
+            try:
+                superuser_password = os.getenv("DJANGO_SUPERUSER_PASSWORD", "admin")
+                superuser.set_password(superuser_password)
+                superuser.save()
+            except Exception as exc:
+                _print(str(exc), string_code="warn", path="initialize")
+                _print("Superuser password changing failed.", string_code="warn", path="initialize")
+            else:
+                _print("Superuser already exists. His password has changed.", string_code="info", path="initialize")
