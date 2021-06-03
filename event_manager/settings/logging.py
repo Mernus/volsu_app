f88cefd985466474ef7b93f5a74cd0190f4b3660
Mernus <mernusv@gmail.com>
2021-06-03 23:39:33 +0300

    Add sentry

--- event_manager/settings/logging.py
+++ event_manager/settings/logging.py
@@ -1,3 +1,6 @@
+import sentry_sdk
+from sentry_sdk.integrations.django import DjangoIntegration
+
 LOGGING = {
     'version': 1,
     'disable_existing_loggers': False,
@@ -32,3 +35,17 @@ LOGGING = {
 }
 
 DEBUG_PROPAGATE_EXCEPTIONS = True
+
+sentry_sdk.init(
+    dsn="https://3e1a5a0ac3a7417e9efae0653b1ace62@o787779.ingest.sentry.io/5799749",
+    integrations=[DjangoIntegration()],
+
+    # Set traces_sample_rate to 1.0 to capture 100%
+    # of transactions for performance monitoring.
+    # We recommend adjusting this value in production.
+    traces_sample_rate=1.0,
+
+    # If you wish to associate users to errors (assuming you are using
+    # django.contrib.auth) you may enable sending PII data.
+    send_default_pii=True
+)
